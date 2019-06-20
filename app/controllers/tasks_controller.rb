b47b78aca8a7eb74d6b8f19585ccd45d30d51ff2
hiroto <hiroto.gackt@gmail.com>
2019-06-20 13:01:24 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in, only: [:create, :new, :update, :destroy]
   def index
       @tasks = Task.all
   end
@@ -13,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
       
       if @task.save
           flash[:success] = "タスクが正常に追加されました"
